e8edb13a7642c2057dde90c21e311f90d7b0b223
Shreyas Agnihotri <sagnihotri@cohesity.com>
2018-08-22 13:06:51 -0700

    Added a powershell script to generate Markdown files for documentation

---
+++ src/Cohesity.Powershell/Generate-Markdown.ps1
@@ -0,0 +1,11 @@
+param (   
+    [string]$ModuleHelpXmlPath = $(throw "-ModuleHelpXmlPath is required. Please provide the path to the PowerShell Module help xml file"),
+    [string]$MarkdownPath      = $(throw "-MarkdownPath is required. Please provide the path to the directory where the markdown files should be generated.")
+)
+
+if (!(Get-Module "PlatyPS")) {
+    Install-Module -Name PlatyPS -Force
+}
+
+Import-Module "PlatyPS"
+New-MarkdownHelp -MamlFile $ModuleHelpXmlPath -OutputFolder $MarkdownPath -Force
