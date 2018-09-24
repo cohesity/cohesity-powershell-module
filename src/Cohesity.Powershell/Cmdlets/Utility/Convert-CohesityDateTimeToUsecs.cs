041d0e6f557a37dd18491ed2a88ceef5d625852a
sgoriparthy <38424201+sgoriparthy@users.noreply.github.com>
2018-09-24 18:29:57 +0530

    Update Convert-CohesityDateTimeToUsecs.cs
    removed unused code

--- src/Cohesity.Powershell/Cmdlets/Utility/Convert-CohesityDateTimeToUsecs.cs
+++ src/Cohesity.Powershell/Cmdlets/Utility/Convert-CohesityDateTimeToUsecs.cs
@@ -35,7 +35,6 @@ namespace Cohesity.Powershell.Cmdlets.Utility
         protected override void ProcessRecord()
         {
             DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            double res = (this.DateTime - UnixEpoch).TotalMilliseconds;
             long microseconds = (this.DateTime.ToUniversalTime() - UnixEpoch).Ticks / (TimeSpan.TicksPerMillisecond / 1000);
             WriteObject(microseconds, true);
         }
