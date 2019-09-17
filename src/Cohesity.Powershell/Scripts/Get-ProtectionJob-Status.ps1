7b709f3c3c69d130ca685b736991d144947a4f88
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2019-09-17 16:01:03 +0530

    Added tabular format for displaying data

--- src/Cohesity.Powershell/Scripts/Get-ProtectionJob-Status.ps1
+++ src/Cohesity.Powershell/Scripts/Get-ProtectionJob-Status.ps1
@@ -18,49 +18,36 @@ function Get-ProtectionJob-Status {
 
         $headers = @{'Authorization'='Bearer '+$token}
         $resp = Invoke-RestMethod -Method 'Get' -Uri $url -Headers $headers -SkipCertificateCheck
-        # $json = $resp | ConvertTo-Json
-        # Write-Host $json
         $jobIdAndName = @{}
         $activeJobIds = New-Object System.Collections.ArrayList
         ForEach ($item in $resp) {
-            #  Write-Host "Active Jobs : " $item.name "," $item.id
              $r = $activeJobIds.Add($item.id)
              $jobIdAndName.Add($item.id,$item.name)
-            #  break;
         }
-        # $url = $server + '/irisservices/api/v1/public/protectionRuns'
-        # $resp = Invoke-RestMethod -Method 'Get' -Uri $url -Headers $headers -SkipCertificateCheck
-        # $processedJobIds = New-Object System.Collections.ArrayList
-        # ForEach ($item in $resp) {
-        #     if($processedJobIds.Contains($item.jobId)) {
-        #         continue
-        #     }
-        #     if($activeJobIds.Contains($item.jobId)) {
-        #         Write-Host "Run status : " $item.jobName,$item.jobId,$item.backupRun.status
-        #         $processedJobIds.Add($item.jobId)
-        #     }
-        # }
-
+        $protectionJobStatusList = @()
         $activeTasks = New-Object System.Collections.ArrayList
         $url = $server + '/irisservices/api/v1/backupjobssummary?_includeTenantInfo=true&allUnderHierarchy=true&includeJobsWithoutRun=true&isDeleted=false&numRuns=1000&onlyReturnBasicSummary=true&onlyReturnJobDescription=false'
         $resp = Invoke-RestMethod -Method 'Get' -Uri $url -Headers $headers -SkipCertificateCheck
         ForEach ($item in $resp) {
             if($activeJobIds.Contains($item.backupJobSummary.jobDescription.jobId)) {
-                # Write-Host $item.backupJobSummary.lastProtectionRun.backupRun.base.publicStatus
                 if($item.backupJobSummary.lastProtectionRun.backupRun.base.publicStatus -notin "kSuccess") {
                     $r = $activeTasks.Add($item.backupJobSummary.lastProtectionRun.backupRun.activeAttempt.base.progressMonitorTaskPath)
-                    # Write-Host $item.backupJobSummary.lastProtectionRun.backupRun.activeAttempt.base.progressMonitorTaskPath
                 } else {
-                    Write-Host "Completed Job : " $jobIdAndName[$item.backupJobSummary.lastProtectionRun.backupRun.base.jobId]
+                    $status = @{
+                        Name=$jobIdAndName[$item.backupJobSummary.lastProtectionRun.backupRun.base.jobId]
+                        Start_Time=$null
+                        Estimated_Time=$null
+                        Completed=100
+                    }
+                    $object = New-Object -TypeName PSObject -Property $status
+                    $protectionJobStatusList += $object
                 }
             }
         }
-
         foreach ($item in $activeTasks) {
             $url = $server + '/irisservices/api/v1/progressMonitors?=excludeSubTasks=true&includeFinishedTasks=true&taskPathVec='+$item
             $resp = Invoke-RestMethod -Method 'Get' -Uri $url -Headers $headers -SkipCertificateCheck
             $task = $resp.resultGroupVec[0].taskVec[0].progress
-            # $remainTimeInMinutes = $task.expectedTimeRemainingSecs/60
             $jobName = ""
             foreach ($jobAttrib in $resp.resultGroupVec[0].taskVec[0].progress.attributeVec) {
                 if($jobAttrib.key -eq "job_name") {
@@ -68,20 +55,26 @@ function Get-ProtectionJob-Status {
                     break
                 }
             }
-            $ts =  [timespan]::fromseconds($task.expectedTimeRemainingSecs)  
-            Write-Host "Job name : " $jobName ", Start time : " $task.startTimeSecs ", Expected time for completion : " ("{0:hh\:mm\:ss\,fff}" -f $ts)" , Completed (%) : " $task.percentFinished
+            $ts =  [timespan]::fromseconds($task.expectedTimeRemainingSecs)
+            $status = @{
+                Name=$jobName
+                Start_Time=$task.startTimeSecs
+                Estimated_Time=("{0:hh\:mm\:ss\,fff}" -f $ts)
+                Completed=$task.percentFinished
+            }
+            $object = New-Object -TypeName PSObject -Property $status
+            $protectionJobStatusList += $object
         }
 
-        # $url = $server + '/irisservices/api/v1/progressMonitors?=excludeSubTasks=true&includeFinishedTasks=true'
-        # $resp = Invoke-RestMethod -Method 'Get' -Uri $url -Headers $headers -SkipCertificateCheck
-        # ForEach ($item in $resp) {
+        $columnWidth = 20
+        $protectionJobStatusList | Format-Table  @{ Label=”PROTECTION JOB”; Expression={$_.Name}; Width=$columnWidth },
+        @{ Label=”STARTED AT”; Expression={$_.Start_Time}; Width=$columnWidth },
+        @{ Label=”ESTIMATED TIME”; Expression={$_.Estimated_Time};Width=$columnWidth },
+        @{ Label=”COMPLETED(%)”; Expression={$_.Completed}; Width=$columnWidth } 
 
-        # }
     }
 
     Process {
-        # Write-Host "Processed!!!!!!!"
-        
     }
 }
 Get-ProtectionJob-Status
