10c8f5442b1a836695bc0be4495060ce8c94d8d1
Ashish Bhat <ashish@cohesity.com>
2019-09-19 14:51:27 -0700

    Adding commandlet Remove-CohesityUser

--- src/Cohesity.Powershell/Scripts/Invoke-RestApi.ps1
+++ src/Cohesity.Powershell/Scripts/Invoke-RestApi.ps1
@@ -7,18 +7,13 @@ function Invoke-RestApi
         $Method,
         $Body
     )
-    
     if ($PSVersionTable.PSVersion.Major -ge 6) {
-        try{
             $result =  Invoke-WebRequest -UseBasicParsing -SkipCertificateCheck @PSBoundParameters
-            } catch {
-                Write-Host "Error in Cmdlet"
-                $result = @{ErrorCode = $_.Exception.Response.StatusCode.value__
-                             ErrorMessage =$_.Exception.Response.ReasonPhrase}
-                return $result
     } else {
         Allow-SelfSignedCertificates
         $result = Invoke-WebRequest -UseBasicParsing @PSBoundParameters
-    
+    }
+
     return ($result.Content | ConvertFrom-Json)
 }
+
