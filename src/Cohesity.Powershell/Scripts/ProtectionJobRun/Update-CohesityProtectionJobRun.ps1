9200182981d7dd98e5236f34e168a407dab396ea
bijayprasad <btiwari.maplelabs@cohesity.com>
2019-11-19 10:14:07 +0530

    Implemented code review comments for updating protection job run
    To support piping
    - Made protection job name optional field
    - Removed the Get-CohesityProtectionJobRun to avoid nested call to
    cmdlete, the pipe iteratively sends out job run id to the commandlete
    - Introduced a parameter $BackupRun, that would store the backup run
    from the pipe

--- src/Cohesity.Powershell/Scripts/ProtectionJobRun/Update-CohesityProtectionJobRun.ps1
+++ src/Cohesity.Powershell/Scripts/ProtectionJobRun/Update-CohesityProtectionJobRun.ps1
@@ -1,17 +1,27 @@
 #### USAGE ####
-#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunId 65675,65163 -ExtendRetention 10
-#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunId 65675 -ExtendRetention 10
+#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunIds 65675,65163 -ExtendRetention 10
+#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunIds 65675 -ExtendRetention 10 (Extend by 10 days)
+#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunIds 65675 -ExtendRetention -3 (Reduce by 3 days)
+#	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -JobRunIds 65675 -ExtendRetention 0 (Mark the snapshot for deletion)
 #	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -ExtendRetention 10
+# 	Update-CohesityProtectionJobRun -ProtectionJobName viewJob -StartTimeUsecs 1573929000000000 -EndTimeUsecs 1574101799999000 -ExtendRetention 10
 ###############
+
 function Update-CohesityProtectionJobRun {
 	[CmdletBinding()]
 	param(
-		[Parameter(Mandatory = $True)]
+		[Parameter(Mandatory = $False)]
 		$ProtectionJobName,
 		[Parameter(Mandatory = $False)]
-		[string[]]$JobRunId = $null,
+		[string[]]$JobRunIds = $null,
+		[Parameter(Mandatory = $False)]
+		[Uint64]$StartTimeUsecs = $null,
+		[Parameter(Mandatory = $False)]
+		[Uint64]$EndTimeUsecs = $null,
 		[Parameter(Mandatory = $True)]
-		$ExtendRetention = $null
+		[Int64]$ExtendRetention = $null,
+		[Parameter(ValueFromPipeline=$True,DontShow=$True)]
+		[object[]]$BackupRun
 	)
 
 	begin {
@@ -21,16 +31,30 @@ function Update-CohesityProtectionJobRun {
 		$session = Get-Content -Path $HOME/.cohesity | ConvertFrom-Json
 		$server = $session.ClusterUri
 		$token = $session.Accesstoken.Accesstoken
+
 	}
 
 	process {
+		if($null -ne $BackupRun) {
+			$JobRunIds = $null
+			foreach($item in $BackupRun) {
+				$JobRunIds += $item.backupRun.jobRunId
+				if($null -eq $ProtectionJobName) {
+					$ProtectionJobName = $item.jobName
+				}
+			}
+		}
+		if($null -eq $ProtectionJobName) {
+			Write-Host "Please provide protection job name"
+			return
+		}
 		#Headers
 		$token = 'Bearer ' + $session.Accesstoken.Accesstoken
 		$headers = @{ "Authorization" = $token }
 		$jobUpdated = 0
 
-                #Fetch the job id for the specified job
 		if ($null -eq $JobName) {
+			#Fetch the job id for the specified job
 			try {
 				$jobUrl = $server + '/irisservices/api/v1/public/protectionJobs'
 				$jobResp = Invoke-RestApi -Method 'Get' -Uri $jobUrl -Headers $headers
@@ -55,6 +79,13 @@ function Update-CohesityProtectionJobRun {
 		#If job exists then collect the job run details for the specific job
 		if ($JobId) {
 			$jobRunUrl = $server + '/irisservices/api/v1/public/protectionRuns?jobId=' + $JobId
+			if ($StartTimeUsecs) {
+				$jobRunUrl = $jobRunUrl + '&startTimeUsecs=' + $StartTimeUsecs
+			}
+			if ($EndTimeUsecs) {
+				$jobRunUrl = $jobRunUrl + '&endTimeUsecs=' + $EndTimeUsecs
+			}
+
 			$JobRuns = Invoke-RestApi -Method 'Get' -Uri $jobRunUrl -Headers $headers
 		} else {
 			Write-Host "Protection job '$ProtectionJobName' doesn't exist."
@@ -62,11 +93,12 @@ function Update-CohesityProtectionJobRun {
 		}
 
 		if ($JobRuns) {
-			if ($null -eq $JobRunId) {
-				$JobRunId += $JobRuns.backupRun.jobRunId
+			if ($null -eq $JobRunIds) {
+				$JobRunIds += $JobRuns.backupRun.jobRunId
 			}
+
 			foreach ($JobRun in $JobRuns) {
-				if ($JobRunId -contains $JobRun.backupRun.jobRunId) {
+				if ($JobRunIds -contains $JobRun.backupRun.jobRunId) {
 					[bool]$snapshotDeleted = $JobRun.backupRun.snapshotsDeleted
 
 					if ($snapshotDeleted -eq $false) {
@@ -98,14 +130,17 @@ function Update-CohesityProtectionJobRun {
 				}
 			}
 
-			if ($jobUpdated -eq $JobRunId.length) {
-				Write-Host "Updated the snapshot retention successfully for the job $ProtectionJobName"
+			if ($jobUpdated -eq $JobRunIds.length) {
+				Write-Host "Updated the snapshot retention for job run ids $JobRunIds, successfully for the job '$ProtectionJobName'"
 			} else {
-				Write-Warning "Some of the snapshot's retention is not updated"
+				Write-Warning "Some of the snapshot's retention is not updated, with job run ids $JobRunIds"
 			}
 		} else {
 			Write-Host "Protection job '$ProtectionJobName' doesn't exist."
 			return
 		}
 	}
+
+	End {
+    }
 }
