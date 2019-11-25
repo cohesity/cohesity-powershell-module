6ed29b6f0bb24b3bc2c6162cf18a42a431d4786a
Bijay Prasad Tiwari <btiwari.maplelabs@cohesity.com>
2019-11-25 00:47:32 -0800

    Pull the latest code from github and start the build process.
    Added script to run pester tests.

---
+++ src/RunTests.ps1
@@ -0,0 +1,12 @@
+$currentPath=pwd
+#Update the Cohesity Powershell module by reinstalling the module
+UnInstall-Module -Name Cohesity.PowerShell -AllVersions  -Verbose
+Install-Module -Name Cohesity.PowerShell -Repository LocalPSRepo -Verbose
+
+#Navigate to SDK-Generation folder
+$targetTestPath = "..\..\sdk-generation\powershell-tests"
+cd $targetTestPath
+
+#Invoke the pester and navigate back to powershell module
+Invoke-Pester -Script .\Access.Management.Tests.ps1 , .\Monitoring.Tests.ps1 , .\Remote.Cluster.Tests.ps1
+cd $currentPath
