f4299719d8f21305556793316e06155f185e449e
Bijay Prasad Tiwari <btiwari.maplelabs@cohesity.com>
2019-11-25 00:56:00 -0800

    Changed name of package from Cohesity.Powershell to Cohesity.PowerShell

--- src/Build.bat
+++ src/Build.bat
@@ -4,9 +4,9 @@ dotnet clean --configuration Release
 dotnet build --configuration Release
 
 
-rmdir /Q /S  .\Cohesity.Powershell\bin\Release\Cohesity.Powershell\
-rmdir /Q /S  .\Cohesity.Powershell\bin\Release\Cohesity.Powershell.Core\
-call c:\Windows\System32\xcopy.exe /E /Q /I .\Cohesity.Powershell\bin\Release\net451 .\Cohesity.Powershell\bin\Release\Cohesity.Powershell
-call c:\Windows\System32\xcopy.exe /E /Q /I .\Cohesity.Powershell\bin\Release\netcoreapp2.0 .\Cohesity.Powershell\bin\Release\Cohesity.Powershell.Core
+rmdir /Q /S  .\Cohesity.Powershell\bin\Release\Cohesity.PowerShell\
+rmdir /Q /S  .\Cohesity.Powershell\bin\Release\Cohesity.PowerShell.Core\
+call c:\Windows\System32\xcopy.exe /E /Q /I .\Cohesity.Powershell\bin\Release\net451 .\Cohesity.Powershell\bin\Release\Cohesity.PowerShell
+call c:\Windows\System32\xcopy.exe /E /Q /I .\Cohesity.Powershell\bin\Release\netcoreapp2.0 .\Cohesity.Powershell\bin\Release\Cohesity.PowerShell.Core
 
 TIMEOUT /T 10
--- src/PublishOnLocalRepo.ps1
+++ src/PublishOnLocalRepo.ps1
@@ -1,8 +1,8 @@
 $localRepoPath="C:\workspace\MyServer\MyServer\Packages\"
-$powerShellPackage=$localRepoPath+"Cohesity.Powershell"
-$powerShellCorePackage=$localRepoPath+"Cohesity.Powershell.Core"
+$powerShellPackage=$localRepoPath+"Cohesity.PowerShell"
+$powerShellCorePackage=$localRepoPath+"Cohesity.PowerShell.Core"
 Remove-Item -Recurse -Force $powerShellPackage
 Remove-Item -Recurse -Force $powerShellCorePackage
 
-Publish-Module -Path '.\Cohesity.Powershell\bin\Release\Cohesity.Powershell\' -Repository LocalPSRepo -NuGetApiKey 'AnyStringWillDo110' -Verbose
-Publish-Module -Path '.\Cohesity.Powershell\bin\Release\Cohesity.Powershell.Core\' -Repository LocalPSRepo -NuGetApiKey 'AnyStringWillDo110Core' -Verbose
+Publish-Module -Path '.\Cohesity.Powershell\bin\Release\Cohesity.PowerShell\' -Repository LocalPSRepo -NuGetApiKey 'AnyStringWillDo110' -Verbose
+Publish-Module -Path '.\Cohesity.Powershell\bin\Release\Cohesity.PowerShell.Core\' -Repository LocalPSRepo -NuGetApiKey 'AnyStringWillDo110Core' -Verbose
