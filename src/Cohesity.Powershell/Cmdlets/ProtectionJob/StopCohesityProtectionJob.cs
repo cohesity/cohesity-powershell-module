65ae746de2c3dcb198e04e9481a26a4b1e2987e5
bijayprasadtiwari <43197022+bijayprasadtiwari@users.noreply.github.com>
2020-02-10 19:46:30 -0800

    Fixed : Stop job run individually for external archival and cloudspin resources (#80)
    Test case/Result		: 	Stop local 	| Stop archival 	| 	Stop replication
    ---------------------------------------------------------------------------------
    Stop local job  		:   	True	|	True			|	True
    Stop archival job 		:		False 	|	True			|	False
    Stop replication		:		False	|	False			|	True

--- src/Cohesity.Powershell/Cmdlets/ProtectionJob/StopCohesityProtectionJob.cs
+++ src/Cohesity.Powershell/Cmdlets/ProtectionJob/StopCohesityProtectionJob.cs
@@ -67,6 +67,31 @@ namespace Cohesity.Powershell.Cmdlets.ProtectionJob
 
         /// <summary>
         /// <para type="description">
+        /// StopArchivalJob is the flag for stopping archival job on external targets.
+        /// Archival External Target (such as Tape or AWS)
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public SwitchParameter StopArchivalJob { get; set; } = false;
+
+        /// <summary>
+        /// <para type="description">
+        /// StopCloudSpinJob is the flag for stopping job at Cloud platform.
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public SwitchParameter StopCloudSpinJob { get; set; } = false;
+
+        /// <summary>
+        /// <para type="description">
+        /// StopReplicationJob is the flag for stopping replication job on external targets.
+        /// </para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public SwitchParameter StopReplicationJob { get; set; } = false;
+
+        /// <summary>
+        /// <para type="description">
         /// Run Id of a protection job run that needs to be cancelled.
         /// If this run id does not match the id of an active run in the protection job, the job run is not cancelled and an error will be returned.
         /// If this is not specified, the last job run id is used.
@@ -90,23 +115,73 @@ namespace Cohesity.Powershell.Cmdlets.ProtectionJob
         /// </summary>
         protected override void ProcessRecord()
         {
+            Model.ProtectionJob currentProtectionjob = null;
             if (!string.IsNullOrWhiteSpace(Name))
             {
                 var job = RestApiCommon.GetProtectionJobByName(Session.ApiClient, Name);
                 Id = (long)job.Id;
+                currentProtectionjob = job;
             }
 
-            if(JobRunId == null)
+            if (JobRunId == null)
             {
                 var job = RestApiCommon.GetProtectionJobById(Session.ApiClient, Id);
                 JobRunId = (long)job.LastRun.BackupRun.JobRunId;
+                currentProtectionjob = job;
             }
 
-            var body = new CancelProtectionJobRunParam {
-                //CopyTaskUid,
-                JobRunId = JobRunId
-            };
-            
+            UniversalId copyTaskUid = null;
+            CopyRun copyRun = null;
+
+            if (StopArchivalJob)
+            {
+                copyRun = currentProtectionjob.LastRun.CopyRun.Find(x => x.Target.Type == SnapshotTargetSettings.TypeEnum.KArchival);
+                if(null == copyRun)
+                {
+                    WriteObject("Could not find the archival task.");
+                    return;
+                }
+
+            }
+            if (StopCloudSpinJob)
+            {
+                copyRun = currentProtectionjob.LastRun.CopyRun.Find(x => x.Target.Type == SnapshotTargetSettings.TypeEnum.KCloudDeploy);
+                if (null == copyRun)
+                {
+                    WriteObject("Could not find the cloud deploy task.");
+                    return;
+                }
+            }
+            if (StopReplicationJob)
+            {
+                copyRun = currentProtectionjob.LastRun.CopyRun.Find(x => x.Target.Type == SnapshotTargetSettings.TypeEnum.KRemote);
+                if (null == copyRun)
+                {
+                    WriteObject("Could not find the replication task.");
+                    return;
+                }
+            }
+
+            object body;
+            if (null != copyRun)
+            {
+                copyTaskUid = copyRun.TaskUid;
+                // When the non-local job is being stopped
+                body = new CancelProtectionJobRunParam
+                {
+                    // The copy task uid and job id would be used for Archival, Cloud spin and Remote targets
+                    CopyTaskUid = copyTaskUid,
+                };
+            }
+            else
+            {
+                // When the local job is being stopped
+                body = new CancelProtectionJobRunParam
+                {
+                    JobRunId = this.JobRunId
+                };
+
+            }
             var url = $"/public/protectionRuns/cancel/{Id.ToString()}";
             Session.ApiClient.Post(url, body);
             WriteObject("Protection Job Run cancelled.");
