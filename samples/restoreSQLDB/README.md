393071c1beb9f32b781221f3cddfb2c7f97ed602
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-05-04 14:14:00 +0530

    Updated README

--- samples/restoreSQLDB/README.md
+++ samples/restoreSQLDB/README.md
@@ -1,11 +1,11 @@
  # Standalone processing individual database
- .\parallel-process-mssql-objects.ps1 -vip 10.2.148.61 -username admin -sourceServer 10.2.100.162 -sourceInstance  SQL2014 -sourceDB SQL2014_DB  -overWrite:$true -wait:$true -progress:$true
+ .\restoreDB.ps1 -vip 10.2.148.61 -username admin -sourceServer 10.2.100.162 -sourceInstance  SQL2014 -sourceDB SQL2014_DB
 
  # Standalone parallel processing all databases inside a single instance
- .\parallel-process-mssql-objects.ps1 -vip 10.2.148.61 -username admin -sourceServer 10.2.100.162 -sourceInstance  SQL2014  -overWrite:$true -wait:$true -progress:$true
- 
- # Piped selection of db and parallel processing of selected dbs 
- .\searchDB.ps1 -vip 10.2.148.61 -username admin -jobName  mssql-59-and-162 | Out-GridView -PassThru | .\parallel-process-mssql-objects.ps1 -vip 10.2.148.61 -username admin -sourceServer 10.2.100.162 -overWrite:$true -wait:$true -progress:$true
- 
- # Use the encapsulated script for piped operation of db
- .\restoreDB.ps1 -vip 10.2.148.61 -username admin -jobName mssql-59-and-162
+ .\restoreDB.ps1 -vip 10.2.148.61 -username admin -sourceServer 10.2.100.162 -sourceInstance  SQL2014
+
+ # Piped selection of db and parallel processing of selected dbs on source server
+ .\restoreDB.ps1 -vip 10.2.148.61 -username admin -jobName  mssql-59-and-162
+
+ # Piped selection of db and parallel processing of selected dbs on target server
+ .\restoreDB.ps1 -vip 10.2.148.61 -username admin -jobName mssql-59-and-162 -restoreType "RESTORE-REMOTE" -targetServer 10.2.100.59 -targetInstance "MYSQLSERVER" -mdfFolder "new-backup"
--- samples/restoreSQLDB/restoreDB.ps1
+++ samples/restoreSQLDB/restoreDB.ps1
@@ -12,15 +12,53 @@ param(
     [string]$targetInstance,
     [Parameter(Mandatory = $true, ParameterSetName = "ForTargetEnabled")]
     [ValidateNotNullOrEmpty()]
-    [string]$mdfFolder
+    [string]$mdfFolder,
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]$sourceServer,
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]$sourceInstance,
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]$sourceDB,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet("RESTORE-LOCAL", "RESTORE-REMOTE")]
+    [string]$restoreType
 )
+Begin {
+    write-host "Restore type selected : " $restoreType
+}
+Process {
+    switch ($restoreType) {
+        "RESTORE-LOCAL" {
+            if($sourceServer -and $sourceInstance -and $sourceDB) {
+                # Standalone processing individual database
+                .\parallel-process-mssql-objects.ps1 -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true `
+                -sourceServer $sourceServer -sourceInstance  $sourceInstance -sourceDB $sourceDB 
+            } elseif ($sourceServer -and $sourceInstance) {
+                # Standalone parallel processing all databases inside a single instance
+                .\parallel-process-mssql-objects.ps1 -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true `
+                -sourceServer $sourceServer -sourceInstance  $sourceInstance
+            } else {
+                # Piped selection of db and parallel processing of selected dbs
+                .\searchDB.ps1 -vip $vip -username $username -jobName  $jobName | Out-GridView -PassThru | `
+                .\parallel-process-mssql-objects.ps1 -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true
+            }
+        }
+        "RESTORE-REMOTE" {
+            if ($targetServer) {
+                # Piped selection of db and parallel processing of selected dbs on target server/instance/folder
+                .\searchDB.ps1 -vip $vip -username $username -jobName  $jobName | Out-GridView -PassThru | `
+                .\parallel-process-mssql-objects.ps1 `
+                 -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true -targetServer $targetServer -targetInstance $targetInstance -mdfFolder $mdfFolder
+            } else {
+                write-host "Please provide target server info"
+            }
+        }
+    }
+}
+End {
+}
 
-if ($targetServer) {
-    .\searchDB.ps1 -vip $vip -username $username -jobName  $jobName | Out-GridView -PassThru | `
-    .\parallel-process-mssql-objects.ps1 `
-     -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true -targetServer $targetServer -targetInstance $targetInstance -mdfFolder $mdfFolder
-}
-else {
-    .\searchDB.ps1 -vip $vip -username $username -jobName  $jobName | Out-GridView -PassThru | `
-    .\parallel-process-mssql-objects.ps1 -vip $vip -username $username  -overWrite:$true -wait:$true -progress:$true
-}
