56cf95740caf2969f153d16bd36fdba01d9031ed
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-05-12 19:58:01 +0530

    Fixed cloning a view from an inactive view

--- src/Cohesity.Powershell/Scripts/View/Copy-CohesityView.ps1
+++ src/Cohesity.Powershell/Scripts/View/Copy-CohesityView.ps1
@@ -13,7 +13,7 @@ function Copy-CohesityView {
         .EXAMPLE
         Copy-CohesityView -TaskName "Task-clone-a-view" -SourceViewName "source-view" -TargetViewName "target-view" -TargetViewDescription "Create a view clone" -JobId 17955 -JobRunId 17956 -StartTime 1582878606980416
     #>
-    [CmdletBinding(DefaultParameterSetName="Default")]
+    [CmdletBinding(DefaultParameterSetName = "Default")]
     param(
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
@@ -33,10 +33,10 @@ function Copy-CohesityView {
         [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
         [string]$QoSPolicy = "Backup Target Low",
-        [Parameter(Mandatory = $true, ParameterSetName="JobRunSpecific")]
+        [Parameter(Mandatory = $true, ParameterSetName = "JobRunSpecific")]
         [ValidateNotNullOrEmpty()]
         [long]$JobRunId = 0,
-        [Parameter(Mandatory = $true, ParameterSetName="JobRunSpecific")]
+        [Parameter(Mandatory = $true, ParameterSetName = "JobRunSpecific")]
         [ValidateNotNullOrEmpty()]
         [long]$StartTime = 0
     )
@@ -49,7 +49,7 @@ function Copy-CohesityView {
         $cohesityToken = $cohesitySession.Accesstoken.Accesstoken
     }
     Process {
-        $sourceView = Get-CohesityView -ViewNames $SourceViewName
+        $sourceView = Get-CohesityView -ViewNames $SourceViewName -IncludeInactive:$true
         if ($null -eq $sourceView) {
             Write-Host "The source view '$SourceViewName' does not exists."
             return
@@ -74,7 +74,7 @@ function Copy-CohesityView {
             $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
             $cloneView = @{
                 sourceViewName = $SourceViewName
-                cloneViewName = $TargetViewName
+                cloneViewName  = $TargetViewName
                 description    = $TargetViewDescription
                 qos            = @{
                     principalName = $QoSPolicy
@@ -84,12 +84,12 @@ function Copy-CohesityView {
                 jobId              = $JobId
                 protectionSourceId = $sourceView.ViewProtection.MagnetoEntityId
             }
-            
-            if($JobRunId -gt 0) {
-                $cloneObject.Add("jobRunId",$JobRunId)
+
+            if ($JobRunId -gt 0) {
+                $cloneObject.Add("jobRunId", $JobRunId)
             }
-            if($StartTime -gt 0) {
-                $cloneObject.Add("startedTimeUsecs",$StartTime)
+            if ($StartTime -gt 0) {
+                $cloneObject.Add("startedTimeUsecs", $StartTime)
             }
             $cloneObjects = @()
             $cloneObjects += $cloneObject
@@ -105,7 +105,6 @@ function Copy-CohesityView {
             $payload = $cloneRequest
 
             $payloadJson = $payload | ConvertTo-Json -Depth 100
-            Write-Host $payloadJson
             $resp = Invoke-RestApi -Method Post -Uri $cohesityClusterURL -Headers $cohesityHeaders -Body $payloadJson
             if ($resp) {
                 $resp
@@ -117,9 +116,73 @@ function Copy-CohesityView {
             }
         }
         else {
-            # operate with the help API
-        }
+            # Clone view from inactive view job run, using private APIs
+            $VIEW_CLONE_ACTION_ID = 5
 
+            $searchURL = $cohesityCluster + '/irisservices/api/v1/searchvms?entityTypes=kView&jobIds=' + $JobId
+            $searchHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+            $searchResult = Invoke-RestApi -Method Get -Uri $searchURL -Headers $searchHeaders
+            if ($null -eq $searchResult) {
+                Write-Host "Could not search view with the job id $JobId"
+                return
+            }
+            $searchedViewDetails = $searchResult.vms | Where-Object { $_.vmDocument.objectId.jobId -eq $JobId -and $_.vmDocument.objectId.entity.id -eq $sourceView.ViewProtection.MagnetoEntityId }
+            if ($null -eq $searchedViewDetails) {
+                Write-Host "Could not find details for view id = "$sourceView.ViewProtection.MagnetoEntityId
+                return
+            }
+            # first instance, find the job run instance and the start time
+            $jobInstanceId = $searchedViewDetails.vmDocument.versions[0].instanceId.jobInstanceId
+            $startTimeUsecs = $searchedViewDetails.vmDocument.versions[0].instanceId.jobStartTimeUsecs
+            $searchedViewDetails.vmDocument.objectId | Add-Member -MemberType NoteProperty -Name jobInstanceId -Value $jobInstanceId
+            $searchedViewDetails.vmDocument.objectId | Add-Member -MemberType NoteProperty -Name startTimeUsecs -Value $startTimeUsecs
+
+            $qos = Get-CohesityQOSPolicy -QOSNames $QoSPolicy
+            if ($null -eq $qos) {
+                Write-Host "The QOS policy '$QoSPolicy' not available"
+                return
+            }
+            # following the swagger model, expecting an array
+            if ("System.Array" -eq $qos.GetType().BaseType.ToString()) {
+                $qos = $qos[0]
+            }
+            $qos | Add-Member -MemberType NoteProperty -Name principalId -Value $qos.id
+            $qos | Add-Member -MemberType NoteProperty -Name principalName -Value $qos.name
+
+            $viewObject = $searchedViewDetails.vmDocument.objectId
+            $viewObjects = @()
+            $viewObjects += $viewObject
+            $payload = @{
+                name               = "Clone-task-" + (Get-Date -Format ss-mm-hh-dd-MMM-yyyy)
+                objects            = $viewObjects
+                viewName           = $TargetViewName
+                action             = $VIEW_CLONE_ACTION_ID
+                viewParams         = @{
+                    sourceViewName = $SourceViewName
+                    cloneViewName  = $TargetViewName
+                    viewBoxId      = $searchedViewDetails.vmDocument.viewBoxId
+                    viewId         = $searchedViewDetails.vmDocument.objectId.entity.id
+                    qos            = $qos
+                }
+                vaultRestoreParams = @{
+                    glacier = @{
+                        retrievalType = "kStandard"
+                    }
+                }
+            }
+        }
+        $payloadJson = $payload | ConvertTo-Json -Depth 100
+        $cohesityClusterURL = $cohesityCluster + '/irisservices/api/v1/clone'
+        $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+        $resp = Invoke-RestApi -Method Post -Uri $cohesityClusterURL -Headers $cohesityHeaders -Body $payloadJson
+        if ($resp) {
+            $resp
+        }
+        else {
+            $errorMsg = "Copy view : Failed to create a copy from inactive view"
+            Write-Host $errorMsg
+            CSLog -Message $errorMsg
+        }
     }
     End {
     }
