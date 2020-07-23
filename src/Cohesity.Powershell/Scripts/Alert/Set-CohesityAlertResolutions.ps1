4c3ec1abcd6c9f8afe4aca05e2fd2e1cfb3f8ba1
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-07-23 15:57:39 +0530

    Lint : Alert

--- src/Cohesity.Powershell/Scripts/Alert/Set-CohesityAlertResolutions.ps1
+++ src/Cohesity.Powershell/Scripts/Alert/Set-CohesityAlertResolutions.ps1
@@ -1,14 +1,14 @@
 function Set-CohesityAlertResolutions {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "High")]
     Param(
         [Parameter(Mandatory = $false)]
-        $ResolutionId=$null,
-        [Parameter(Mandatory = $true,ValueFromPipelineByPropertyName=$True)]
+        $ResolutionId = $null,
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $True)]
         [alias("Id")][string[]]$AlertIds,
         [Parameter(Mandatory = $false)]
-        $ResolutionSummary=$null,
+        $ResolutionSummary = $null,
         [Parameter(Mandatory = $false)]
-        $ResolutionDetails=$null
+        $ResolutionDetails = $null
     )
     Begin {
         if (-not (Test-Path -Path "$HOME/.cohesity")) {
@@ -22,36 +22,38 @@ function Set-CohesityAlertResolutions {
     }
 
     Process {
-        if($null -eq $ResolutionId) {
-            if($null -eq $ResolutionSummary) {
-                $ResolutionSummary = "Resolved alerts through powershell cmdlets" #this is a mandatory field in the payload
-            }
-            $url = $server + '/irisservices/api/v1/public/alertResolutions'
-
-            $headers = @{'Authorization'='Bearer '+$token}
-            $payload = @{
-                alertIdList=@($AlertIds)
-                resolutionDetails=@{
-                    resolutionDetails=$ResolutionDetails
-                    resolutionSummary=$ResolutionSummary
+        if ($PSCmdlet.ShouldProcess($AlertIds)) {
+            if ($null -eq $ResolutionId) {
+                if ($null -eq $ResolutionSummary) {
+                    $ResolutionSummary = "Resolved alerts through powershell cmdlets" #this is a mandatory field in the payload
                 }
-            }
-            $payloadJson = $payload  | ConvertTo-Json
-            $resp = Invoke-RestApi -Method 'Post' -Uri $url -Headers $headers -Body $payloadJson 
-            Write-Host "Successfully created, the resolution id ="$resp.resolutionDetails.resolutionId
-        
-        } else {
-            $url = $server + '/irisservices/api/v1/public/alertResolutions/'+$ResolutionId
+                $url = $server + '/irisservices/api/v1/public/alertResolutions'
 
-            $headers = @{'Authorization'='Bearer '+$token}
-            $payload = @{
-                alertIdList=@($AlertIds)
+                $headers = @{'Authorization' = 'Bearer ' + $token }
+                $payload = @{
+                    alertIdList       = @($AlertIds)
+                    resolutionDetails = @{
+                        resolutionDetails = $ResolutionDetails
+                        resolutionSummary = $ResolutionSummary
+                    }
+                }
+                $payloadJson = $payload  | ConvertTo-Json
+                $resp = Invoke-RestApi -Method 'Post' -Uri $url -Headers $headers -Body $payloadJson
+                Write-Output "Successfully created, the resolution id ="$resp.resolutionDetails.resolutionId
+
             }
-            $payloadJson = $payload  | ConvertTo-Json
-            $resp = Invoke-RestApi -Method 'Put' -Uri $url -Headers $headers -Body $payloadJson
-            Write-Host "Successfully updated, the resolution id ="$resp.resolutionDetails.resolutionId
+            else {
+                $url = $server + '/irisservices/api/v1/public/alertResolutions/' + $ResolutionId
+
+                $headers = @{'Authorization' = 'Bearer ' + $token }
+                $payload = @{
+                    alertIdList = @($AlertIds)
+                }
+                $payloadJson = $payload  | ConvertTo-Json
+                $resp = Invoke-RestApi -Method 'Put' -Uri $url -Headers $headers -Body $payloadJson
+                Write-Output "Successfully updated, the resolution id ="$resp.resolutionDetails.resolutionId
+            }
         }
-
     }
     End {
     }
