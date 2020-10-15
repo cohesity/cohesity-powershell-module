17aa69fd83bae7206e42a531c25d6df07a16328e
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-10-15 15:15:00 +0530

    Replaced the C# based cmdlet 'Get-CohesityProtectionSourceObject' with script based

---
+++ src/Cohesity.Powershell/Scripts/ProtectionSource/Get-CohesityProtectionSourceObject.ps1
@@ -0,0 +1,91 @@
+function Get-CohesityProtectionSourceObject {
+    <#
+        .SYNOPSIS
+        Get protection source object.
+        .DESCRIPTION
+        The Get-CohesityProtectionSourceObject function is used to get protection source object.
+        .NOTES
+        Published by Cohesity
+        .LINK
+        https://cohesity.github.io/cohesity-powershell-module/#/README
+        .EXAMPLE
+        Get-CohesityProtectionSourceObject -Environments kPhysical
+        .EXAMPLE
+        Get-CohesityProtectionSourceObject -Id 1234
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$IncludeDatastores,
+        [Parameter(Mandatory = $false)]
+        [switch]$IncludeNetworks,
+        [Parameter(Mandatory = $false)]
+        [switch]$IncludeVMFolders,
+        [Parameter(Mandatory = $false)]
+        [Cohesity.Model.ProtectionSource+EnvironmentEnum[]]$Environments,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [long]$Id,
+        [Parameter(Mandatory = $false)]
+        [string[]]$ExcludeTypes
+    )
+
+    Begin {
+        if (-not (Test-Path -Path "$HOME/.cohesity")) {
+            throw "Failed to authenticate. Please connect to the Cohesity Cluster using 'Connect-CohesityCluster'"
+        }
+        $cohesitySession = Get-Content -Path $HOME/.cohesity | ConvertFrom-Json
+        $cohesityServer = $cohesitySession.ClusterUri
+        $cohesityToken = $cohesitySession.Accesstoken.Accesstoken
+    }
+
+    Process {
+        $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+        if($Id) {
+            $cohesityUrl = $cohesityServer + '/irisservices/api/v1/public/protectionSources/objects/' + $Id.ToString()
+            $resp = Invoke-RestApi -Method Get -Uri $cohesityUrl -Headers $cohesityHeaders
+            $resp = @($resp)
+            $resp
+        }
+        else {
+            $url = '/irisservices/api/v1/public/protectionSources'
+            $filter = ""
+            if($IncludeDatastores.IsPresent) {
+                $filter += "includeDatastores=true"
+            }
+            if ($IncludeNetworks.IsPresent) {
+                if($filter -ne "") {
+                    $filter += "&"
+                }
+                $filter += "includeNetworks=true"
+            }
+            if ($IncludeVMFolders.IsPresent) {
+                if($filter -ne "") {
+                    $filter += "&"
+                }
+                $filter += "includeVMFolders=true"
+            }
+            if (-not $Environments) {
+                if($filter -ne "") {
+                    $filter += "&"
+                }
+                $filter += "environments="+($Environments -join ",")
+            }
+            if ($ExcludeTypes.IsPresent) {
+                if($filter -ne "") {
+                    $filter += "&"
+                }
+                $filter += "excludeTypes=true"
+            }
+            if($filter -ne "") {
+                $url += ("&" + $filter)
+            }
+
+            $cohesityUrl = $cohesityServer + $url
+            $resp = Invoke-RestApi -Method Get -Uri $cohesityUrl -Headers $cohesityHeaders
+            $resp
+        }
+    }
+
+    End {
+    }
+}
