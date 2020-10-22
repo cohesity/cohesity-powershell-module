58a1aac07b309d26d9c4d9298564e127fed2d9cb
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-10-22 10:56:23 +0530

    Migrated cmdlet Set-CohesityProtectionPolicy to script based

---
+++ src/Cohesity.Powershell/Scripts/ProtectionPolicy/Set-CohesityProtectionPolicy.ps1
@@ -0,0 +1,57 @@
+function Set-CohesityProtectionPolicy {
+    <#
+        .SYNOPSIS
+        Get protection source.
+        .DESCRIPTION
+        The Set-CohesityProtectionPolicy function is used to set protection policy.
+        .NOTES
+        Published by Cohesity
+        .LINK
+        https://cohesity.github.io/cohesity-powershell-module/#/README
+        .EXAMPLE
+        Set-CohesityProtectionPolicy -ProtectionPolicy Test-Policy -Confirm:$false
+        .EXAMPLE
+        $result = Get-CohesityProtectionPolicy -Name Test-Policy
+        Set-CohesityProtectionPolicy -Names $result
+        .EXAMPLE
+        $result = Get-CohesityProtectionPolicy -Name Test-Policy
+        $result.name = "Test-Policy-updated"
+        $result | Set-CohesityProtectionPolicy
+    #>
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "High")]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [object]$ProtectionPolicy
+    )
+
+    Begin {
+        if (-not (Test-Path -Path "$HOME/.cohesity")) {
+            throw "Failed to authenticate. Please connect to the Cohesity Cluster using 'Connect-CohesityCluster'"
+        }
+        $cohesitySession = Get-Content -Path $HOME/.cohesity | ConvertFrom-Json
+        $cohesityServer = $cohesitySession.ClusterUri
+        $cohesityToken = $cohesitySession.Accesstoken.Accesstoken
+    }
+
+    Process {
+        $protectionPolicyName = $ProtectionPolicy.name
+        if ($PSCmdlet.ShouldProcess($protectionPolicyName)) {
+            $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+            $url = '/irisservices/api/v1/public/protectionPolicies/' + $ProtectionPolicy.id
+            $cohesityUrl = $cohesityServer + $url
+            $payloadJson = ($ProtectionPolicy | ConvertTo-Json -Depth 100)
+            $resp = Invoke-RestApi -Method Put -Uri $cohesityUrl -Headers $cohesityHeaders -Body $payloadJson
+            if($resp) {
+                $resp
+            }
+            else {
+                $errorMsg = "Protection Policy : $protectionPolicyName, Failed to update"
+                Write-Output $errorMsg
+                CSLog -Message $errorMsg
+            }
+        }
+    }
+
+    End {
+    }
+}
