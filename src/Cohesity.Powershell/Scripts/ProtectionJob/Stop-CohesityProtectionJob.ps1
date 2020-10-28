620bc873ba15801bfac83722fb85ad59249d73d6
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-10-28 19:44:00 +0530

    Script analyzer fixes

--- src/Cohesity.Powershell/Scripts/ProtectionJob/Stop-CohesityProtectionJob.ps1
+++ src/Cohesity.Powershell/Scripts/ProtectionJob/Stop-CohesityProtectionJob.ps1
@@ -13,13 +13,13 @@ function Stop-CohesityProtectionJob {
         Stop-CohesityProtectionJob -Id 78773 -JobRunId 85510
     #>
     [OutputType('System.Object')]
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
     Param(
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "ById")]
         [ValidateRange(1, [long]::MaxValue)]
         [long]$Id,
         [Parameter(Mandatory = $true, ParameterSetName = "ByName")]
-        [ValidateNotNullOrEmpty]
+        [ValidateNotNullOrEmpty()]
         [string]$Name,
         [Parameter(Mandatory = $false)]
         [switch]$StopArchivalJob,
@@ -60,60 +60,54 @@ function Stop-CohesityProtectionJob {
             $JobRunId = $job.LastRun.BackupRun.JobRunId
             $currentProtectionjob = $job
         }
+        if ($PSCmdlet.ShouldProcess($Id)) {
+            $copyRun = $null;
 
-        $copyRun = $null;
-
-        if ($StopArchivalJob.IsPresent)
-        {
-            $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object{$_.Target.Type -eq "kArchival"}
-            if(-not $copyRun)
-            {
-                Write-Output "Could not find the archival task."
-                return
+            if ($StopArchivalJob.IsPresent) {
+                $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object { $_.Target.Type -eq "kArchival" }
+                if (-not $copyRun) {
+                    Write-Output "Could not find the archival task."
+                    return
+                }
             }
-        }
-        if ($StopCloudSpinJob.IsPresent)
-        {
-            $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object{$_.Target.Type -eq "kCloudDeploy"}
-            if (-not $copyRun)
-            {
-                Write-Output "Could not find the cloud deploy task."
-                return
+            if ($StopCloudSpinJob.IsPresent) {
+                $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object { $_.Target.Type -eq "kCloudDeploy" }
+                if (-not $copyRun) {
+                    Write-Output "Could not find the cloud deploy task."
+                    return
+                }
             }
-        }
-        if ($StopReplicationJob.IsPresent)
-        {
-            $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object{$_.Target.Type -eq "kRemote"}
-            if (-not $copyRun)
-            {
-                Write-Output "Could not find the replication task."
-                return
+            if ($StopReplicationJob.IsPresent) {
+                $copyRun = $currentProtectionjob.LastRun.CopyRun | where-object { $_.Target.Type -eq "kRemote" }
+                if (-not $copyRun) {
+                    Write-Output "Could not find the replication task."
+                    return
+                }
             }
-        }
 
-        $payload = [Cohesity.Model.CancelProtectionJobRunParam]::new()
-        if ($copyRun)
-        {
-            $taskUid = [Cohesity.Model.UniversalId]::new()
-            $taskUid.Id = $copyRun.TaskUid.Id
-            $taskUid.clusterId = $copyRun.TaskUid.clusterId
-            $taskUid.clusterIncarnationId = $copyRun.TaskUid.clusterIncarnationId
-            # When the non-local job is being stopped
-            # The copy task uid and job id would be used for Archival, Cloud spin and Remote targets
-            $payload.CopyTaskUid = $taskUid
-        }
-        else
-        {
-            # When the local job is being stopped
-            $payload.JobRunId = $JobRunId
-        }
+            $payload = [Cohesity.Model.CancelProtectionJobRunParam]::new()
+            if ($copyRun) {
+                $taskUid = [Cohesity.Model.UniversalId]::new()
+                $taskUid.Id = $copyRun.TaskUid.Id
+                $taskUid.clusterId = $copyRun.TaskUid.clusterId
+                $taskUid.clusterIncarnationId = $copyRun.TaskUid.clusterIncarnationId
+                # When the non-local job is being stopped
+                # The copy task uid and job id would be used for Archival, Cloud spin and Remote targets
+                $payload.CopyTaskUid = $taskUid
+            }
+            else {
+                # When the local job is being stopped
+                $payload.JobRunId = $JobRunId
+            }
 
-        $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
-        $url = '/irisservices/api/v1/public/protectionRuns/cancel/' + $Id
-        $cohesityUrl = $cohesityServer + $url
-        $payloadJson = $payload | ConvertTo-Json -Depth 100
-        $resp = Invoke-RestApi -Method Post -Uri $cohesityUrl -Headers $cohesityHeaders -Body $payloadJson
-        Write-Output "Protection Job Run cancelled."
+            $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+            $url = '/irisservices/api/v1/public/protectionRuns/cancel/' + $Id
+            $cohesityUrl = $cohesityServer + $url
+            $payloadJson = $payload | ConvertTo-Json -Depth 100
+            $resp = Invoke-RestApi -Method Post -Uri $cohesityUrl -Headers $cohesityHeaders -Body $payloadJson
+            $resp | Out-Null
+            Write-Output "Protection Job Run cancelled."
+        }
     }
 
     End {
