208e27f83e19bf083f0e3d94bca71794a97bab58
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2020-11-27 14:21:35 +0530

    Added cmdlet Add-CohesityProtectionSourceForPrincipal

--- src/Cohesity.Powershell/Cohesity.PowerShell.Core.psd1
+++ src/Cohesity.Powershell/Cohesity.PowerShell.Core.psd1
@@ -67,6 +67,7 @@ NestedModules = @('Cohesity.PowerShell.psm1')
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
                       'Add-CohesityExternalClient',
+                      'Add-CohesityProtectionSourceForPrincipal',
                       'Copy-CohesityView',
                       'Get-CohesityActiveDirectory',
                       'Get-CohesityCmdletConfig',
---
+++ src/Cohesity.Powershell/Scripts/Principals/Add-CohesityProtectionSourceForPrincipal.ps1
@@ -0,0 +1,103 @@
+function Add-CohesityProtectionSourceForPrincipal {
+    <#
+        .SYNOPSIS
+        Specify the security identifier (SID) of the principal to grant access permissions for protectio source.
+        .DESCRIPTION
+        Add Protection Sources and Views that the specified principal has permissions to access.
+
+        .NOTES
+        Published by Cohesity
+        .LINK
+        https://cohesity.github.io/cohesity-powershell-module/#/README
+        .EXAMPLE
+        Add-CohesityProtectionSourceForPrincipal -PrincipalType "GROUP" -PrincipalName user-group1 -ProtectionSourceObjectIds 121,344
+        Add protection sources ids 121 and 344 to grant access to user-group1
+        .EXAMPLE
+        Add-CohesityProtectionSourceForPrincipal -PrincipalType "USER" -PrincipalName user1 -ProtectionSourceObjectIds 121,344
+        Add protection sources ids 121 and 344 to grant access to user1
+        .EXAMPLE
+        Add-CohesityProtectionSourceForPrincipal -PrincipalType "USER" -PrincipalName user1 -ViewNames view1, view2
+        Add views view1 and view2 to grant access to user1
+    #>
+    [CmdletBinding(DefaultParameterSetName = "DefaultParameters", SupportsShouldProcess = $True, ConfirmImpact = "High")]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("USER", "GROUP")]
+        [string]$PrincipalType,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$PrincipalName,
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [long[]]$ProtectionSourceObjectIds,
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ViewNames
+    )
+
+    Begin {
+        if (-not (Test-Path -Path "$HOME/.cohesity")) {
+            throw "Failed to authenticate. Please connect to the Cohesity Cluster using 'Connect-CohesityCluster'"
+        }
+        $cohesitySession = Get-Content -Path $HOME/.cohesity | ConvertFrom-Json
+        $cohesityCluster = $cohesitySession.ClusterUri
+        $cohesityToken = $cohesitySession.Accesstoken.Accesstoken
+    }
+
+    Process {
+        if((-not $ProtectionSourceObjectIds) -and (-not $ViewNames)) {
+            Write-Output "Please provide the protection source ids and/or the view names"
+            return
+        }
+        $principalDetail = Get-CohesityProtectionSourceForPrincipal -PrincipalType $PrincipalType -PrincipalName $PrincipalName
+        if (-not $principalDetail) {
+            return
+        }
+        if ($ProtectionSourceObjectIds) {
+            $protectionSourceObjects = Get-CohesityProtectionSourceObject
+            foreach($Id in $ProtectionSourceObjectIds) {
+                if ($protectionSourceObjects.Id -notcontains $Id) { 
+                    Write-Output "Protection source id '$Id' not found"
+                    return
+                }
+            }
+        }
+        if($ViewNames) {
+            $viewObjects = Get-CohesityView
+            foreach($viewName in $ViewNames) {
+                if($viewObjects.Name -notcontains $viewName) {
+                    Write-Output "View name '$viewName' not found"
+                    return
+                }
+            }
+        }
+
+        if ($PSCmdlet.ShouldProcess($PrincipalName)) {
+            $cohesityClusterURL = $cohesityCluster + '/irisservices/api/v1/public/principals/protectionSources'
+            $cohesityHeaders = @{'Authorization' = 'Bearer ' + $cohesityToken }
+
+            $sourcesForPrincipalObject = @{
+                protectionSourceIds = @($ProtectionSourceObjectIds)
+                sid = $principalDetail.Sid
+                viewNames = @($ViewNames)
+            }
+            $payload = @{
+                sourcesForPrincipals = @($sourcesForPrincipalObject)
+              }
+            $payloadJson = $payload | ConvertTo-Json -Depth 100
+            Invoke-RestApi -Method Put -Uri $cohesityClusterURL -Headers $cohesityHeaders -Body $payloadJson
+            if(204 -eq $Global:CohesityAPIResponse.StatusCode) {
+                @{Response = "Success"; Method = "Put"; }
+            }
+            else {
+                $errorMsg = "Protection source and view permission : Failed to add"
+                Write-Output $errorMsg
+                CSLog -Message $errorMsg
+            }
+        }
+    }
+
+    End {
+    }
+}
