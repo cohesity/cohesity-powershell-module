65cac09cee230ff3c52e5e6fd220c59fd14f62f0
bijayprasadtiwari <btiwari.maplelabs@cohesity.com>
2020-11-30 14:10:45 +0530

    Identify the module name to generate the doc files

--- src/Generate-CohesityDoc.ps1
+++ src/Generate-CohesityDoc.ps1
@@ -7,7 +7,18 @@ Param(
 
 Begin {
 
-	New-MarkdownHelp -Module Cohesity.Powershell.Core -OutputFolder $OutputFolder -Force
+	$moduleName = "Cohesity.PowerShell"
+	$resp = Get-InstalledModule
+	$installedPackage = $resp | Where-Object { $_.Name -contains "Cohesity.PowerShell" }
+	if (-not $installedPackage) {
+		$moduleName = "Cohesity.PowerShell.Core"
+		$installedPackage = $resp | Where-Object { $_.Name -contains "Cohesity.PowerShell.Core" }
+	}
+	if (-not $installedPackage) {
+		write-output "No Cohesity module found"
+		return
+	}
+	New-MarkdownHelp -Module $moduleName -OutputFolder $OutputFolder -Force
 	$mdFiles = @( Get-ChildItem -Recurse -Path $OutputFolder\*.md -ErrorAction SilentlyContinue )
 	foreach($mdFile in $mdFiles) {
 		mv $mdFile.FullName $mdFile.FullName.ToLower()
