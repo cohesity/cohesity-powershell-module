6c2d2319976888a3305f1947d44f42cc002e29ba
Bijay Tiwari <cohesity@view-btiwari.maplelabs-ubu>
2021-03-02 10:54:44 +0530

    Fixed vmware object cluster name

--- samples/Helios/HeliosWebRequest.psm1
+++ samples/Helios/HeliosWebRequest.psm1
@@ -8,43 +8,47 @@ function HeliosWebRequest {
     )
     Begin {}
     Process {
-
-        If ($PSVersionTable.PSVersion.Major -ge 6) {
-            $result = Invoke-WebRequest -UseBasicParsing -SkipCertificateCheck -Method $Method -Uri $Uri -Headers $Headers
-        }
-        else {
-            # ignore self-signed server certificates
-            New-Object System.Net.WebClient | Out-Null;
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            Add-Type @"
-        using System;
-        using System.Net;
-        using System.Net.Security;
-        using System.Security.Cryptography.X509Certificates;
-        public class ServerCertificateValidationCallback
-        {
-            public static void Ignore()
-            {
-                ServicePointManager.ServerCertificateValidationCallback +=
-                    delegate
-                    (
-                        Object obj,
-                        X509Certificate certificate,
-                        X509Chain chain,
-                        SslPolicyErrors errors
-                    )
+            try {
+                If ($PSVersionTable.PSVersion.Major -ge 6) {
+                    $result = Invoke-WebRequest -UseBasicParsing -SkipCertificateCheck -Method $Method -Uri $Uri -Headers $Headers
+                }
+                else {
+                    # ignore self-signed server certificates
+                    New-Object System.Net.WebClient | Out-Null;
+                    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                    Add-Type @"
+                using System;
+                using System.Net;
+                using System.Net.Security;
+                using System.Security.Cryptography.X509Certificates;
+                public class ServerCertificateValidationCallback
+                {
+                    public static void Ignore()
                     {
-                        return true;
-                    };
-            }
-        }
+                        ServicePointManager.ServerCertificateValidationCallback +=
+                            delegate
+                            (
+                                Object obj,
+                                X509Certificate certificate,
+                                X509Chain chain,
+                                SslPolicyErrors errors
+                            )
+                            {
+                                return true;
+                            };
+                    }
+                }
 "@
-            [ServerCertificateValidationCallback]::Ignore();
-            $result = Invoke-WebRequest -UseBasicParsing -Method $Method -Uri $Uri -Headers $Headers
-        }
+                    [ServerCertificateValidationCallback]::Ignore();
+                    $result = Invoke-WebRequest -UseBasicParsing -Method $Method -Uri $Uri -Headers $Headers
+                }
 
-        ($result.Content | ConvertFrom-Json)
-    
+                ($result.Content | ConvertFrom-Json)
+            }
+            catch {
+                Write-Output "Exception : " $_.ErrorDetails.Message
+            }
+
     }
     End {}
 
