ed04f9320ea0965a8c599e2dbc8ec7b460f7ef1b
bijayprasadtiwari <43197022+bijayprasadtiwari@users.noreply.github.com>
2021-04-26 00:59:41 -0700

    Release 168 1 (#153)
    * Fixed restoring mssql secondary data files
    
    * Updated document for restore remote MSSQL secondary file
    
    * Added report generator for ut core test as well
    
    Co-authored-by: btiwari.maplelabs <btiwari.maplelabs@u18-101660-01.corp.cohesity.com>

--- src/RunCoreTests.ps1
+++ src/RunCoreTests.ps1
@@ -4,6 +4,14 @@ $currentPath=pwd
 UnInstall-Module -Name Cohesity.PowerShell.Core -AllVersions  -Verbose
 Install-Module -Name Cohesity.PowerShell.Core -Repository LocalPSRepo -Verbose
 
+# create a fresh unit test report directory
+$utReportDirectory = $currentPath.ToString() + "\UTReportCore\"
+if ([System.IO.Directory]::Exists($utReportDirectory)) {
+	Remove-Item -Path $utReportDirectory -Force -Confirm:$false
+}
+# create a folder for accumulating unit test report
+New-Item -Path $utReportDirectory -ItemType "directory" -Force | Out-Null
+
 
 #Navigate to SDK-Generation folder
 $targetTestPath = "..\..\sdk-generation\powershell-tests"
@@ -22,9 +30,13 @@ foreach($config in $configFiles) {
 
     Copy-Item -Path $config.fullname -Destination ".\config.ini"  -Force
 
-	$reportFileName = $currentPath.ToString() + "\Win-PS-Core-" + $newTag + "-" + (Get-Date -Format "dddd-MM-dd-yyyy-HH-mm-ss").ToString() + ".xml"
+	$reportFileName = $utReportDirectory + "\Win-PS-Core-" + $newTag + "-" + (Get-Date -Format "dddd-MM-dd-yyyy-HH-mm-ss").ToString() + ".xml"
 	#Invoke the pester
 	Invoke-Pester -Tag $tags -OutputFormat  NUnitXml -OutputFile $reportFileName
 }
 
 cd $currentPath
+
+
+# run the report unit to convert the unit test report to a readable format
+.\ReportUnit.exe $utReportDirectory $utReportDirectory
