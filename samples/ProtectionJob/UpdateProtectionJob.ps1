f44f6638a540f8ddd9674e2988495f5aeccd2907
bijayprasadtiwari <43197022+bijayprasadtiwari@users.noreply.github.com>
2021-10-06 14:22:07 +0530

    Create UpdateProtectionJob.ps1
    Adding a sample script

---
+++ samples/ProtectionJob/UpdateProtectionJob.ps1
@@ -0,0 +1,14 @@
+# get the job
+$job = Get-CohesityProtectionJob -Names job-vmware
+
+# set the alerting policy
+$job.alertingPolicy = @("kFailure","kSlaViolation")
+
+# create the alerting config object
+$config = [Cohesity.Model.AlertingConfig]::new()
+$config.EmailAddresses = @("abc@cohesity.com","xyz@cohesity.com")
+# append the member 'AlertingConfig' if it does not exists in $job
+$job | Add-Member -NotePropertyName AlertingConfig -NotePropertyValue $config
+
+# the job is ready to be updated
+$job | Set-CohesityProtectionJob -Confirm:$false
