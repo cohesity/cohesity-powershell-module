90a0d382e5911d07708e246c0ec1febaf4536e7c
bijayprasadtiwari <btiwari.maplelabs@cohesity.com>
2021-10-18 11:10:22 +0530

    Added aws registration for iamUser

--- src/Cohesity.Powershell/Scripts/ProtectionSource/Register-CohesityProtectionSourceAWS.ps1
+++ src/Cohesity.Powershell/Scripts/ProtectionSource/Register-CohesityProtectionSourceAWS.ps1
@@ -10,6 +10,13 @@ function Register-CohesityProtectionSourceAWS {
         https://cohesity.github.io/cohesity-powershell-module/#/README
         .EXAMPLE
         Register-CohesityProtectionSourceAWS -AccessKey "access-key" -SecretAccessKey "secret-key" -ARN "aws-arn"
+        .EXAMPLE
+        Register-CohesityProtectionSourceAWS -AccessKey access-key -SecretAccessKey "secret-key" -ARN "aws-arn" -FleetSubnetType kSourceVM -FleetTags $tags
+        Construct the tags as follows,
+        $tags = @()
+        $tags += @{key="bjt1";value="bjv1"}
+        $tags += @{key="bjt2";value="bjv2"}
+
     #>
   [CmdletBinding()]
   Param(
@@ -17,6 +24,9 @@ function Register-CohesityProtectionSourceAWS {
     [ValidateNotNullOrEmpty()]
     # Specifies the access key.
     [String]$AccessKey,
+    [Parameter(Mandatory = $false)]
+    # Specifies amazon resource name.
+    [String]$AmazonResourceName,
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     # Specifies the secret access key.
@@ -24,7 +34,29 @@ function Register-CohesityProtectionSourceAWS {
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     # Specifies the ARN.
-    [String]$ARN
+    [String]$ARN,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kUseIAMUser","kUseIAMRole")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the authentication method.
+    [String]$AuthMethod = "kUseIAMUser",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kIAMUser","kRegion","kAvailabilityZone","kEC2Instance","kVPC","kSubnet","kNetworkSecurityGroup","kInstanceType","kKeyPair","kTag","kRDSOptionGroup","kRDSParameterGroup","kRDSInstance","kRDSSubnet","kRDSTag","kAuroraCluster")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the AWS type.
+    [String]$AWSType = "kIAMUser",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kAWSCommercial","kAWSGovCloud")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the subscription type.
+    [String]$SubscriptionType = "kAWSCommercial",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kCluster","kSourceVM","kCustom")]
+    # Fleet settings, fleet subnet type.
+    [String]$FleetSubnetType,
+    [Parameter(Mandatory = $false)]
+    # Fleet settings, fleet tags.
+    [object[]]$FleetTags
   )
 
   Begin {
@@ -40,15 +72,24 @@ function Register-CohesityProtectionSourceAWS {
     $awsRegistrationParameters = @{
       awsCredentials = @{
         accessKey       = $AccessKey
-        authMethod      = "kUseIAMUser"
-        awsType         = "kIAMUser"
+        amazonResourceName = $AmazonResourceName
+        authMethod      = $AuthMethod
+        awsType         = $AWSType
         iamRoleArn      = $ARN
         secretAccessKey = $SecretAccessKey
+        subscriptionType = $SubscriptionType
       }
       environment    = "kAWS"
     }
+    if ($FleetSubnetType) {
+        $awsFleetParams = @{
+            fleetSubnetType = $FleetSubnetType
+            fleetTags = $FleetTags
+        }
+        $awsRegistrationParameters | Add-Member -NotePropertyName awsFleetParams -NotePropertyValue $awsFleetParams
+    }
 
-    $request = $awsRegistrationParameters | ConvertTo-Json
+    $request = $awsRegistrationParameters | ConvertTo-Json -Depth 100
     $result = Invoke-RestApi -Method Post -Headers $headers -Uri $uri -Body $request
     $result
   }
