fe76fda9fea731851f04a41786baad1e2e5c36ad
bijayprasadtiwari <43197022+bijayprasadtiwari@users.noreply.github.com>
2021-11-22 13:30:56 +0530

    Release 175 0 (#168)
    * Added fleet settings for aws registration
    
    * iamrole auth method added
    
    * Added fleet attribute for iamrole
    
    * Added example aws registration
    
    * Unit test fix
    
    * Fixed fleet settings for iamRole
    
    * Updated documentation

--- docs/cmdlets-reference/register-cohesityprotectionsourceaws.md
+++ docs/cmdlets-reference/register-cohesityprotectionsourceaws.md
@@ -6,8 +6,10 @@ Registers a new AWS protection source with the Cohesity Cluster.
 ## SYNTAX
 
 ```
-Register-CohesityProtectionSourceAWS [-AccessKey] <String> [-SecretAccessKey] <String> [-ARN] <String>
- [<CommonParameters>]
+Register-CohesityProtectionSourceAWS [[-AccessKey] <String>] [[-AmazonResourceName] <String>]
+ [[-SecretAccessKey] <String>] [[-ARN] <String>] [[-AuthMethod] <String>] [[-AWSType] <String>]
+ [[-SubscriptionType] <String>] [[-FleetSubnetType] <String>] [[-FleetTags] <Object[]>] [[-AccountId] <String>]
+ [[-IamRoleName] <String>] [<CommonParameters>]
 ```
 
 ## DESCRIPTION
@@ -20,6 +22,26 @@ Registers a new AWS protection source with the Cohesity Cluster.
 Register-CohesityProtectionSourceAWS -AccessKey "access-key" -SecretAccessKey "secret-key" -ARN "aws-arn"
 ```
 
+### EXAMPLE 2
+```
+Register-CohesityProtectionSourceAWS -AccessKey access-key -SecretAccessKey "secret-key" -ARN "aws-arn" -FleetSubnetType kSourceVM -FleetTags $tags
+```
+
+Construct the tags as follows,
+$tags = @()
+$tags += @{key="bjt1";value="bjv1"}
+$tags += @{key="bjt2";value="bjv2"}
+
+### EXAMPLE 3
+```
+Register-CohesityProtectionSourceAWS -AuthMethod kUseIAMRole -IamRoleName "role-name" -AccountId "12321432" -FleetSubnetType kSourceVM -FleetTags $tags
+```
+
+Register AWS protection source with IAmRole authentication method, construct the tags as follows,
+$tags = @()
+$tags += @{key="bjt1";value="bjv1"}
+$tags += @{key="bjt2";value="bjv2"}
+
 ## PARAMETERS
 
 ### -AccessKey
@@ -30,8 +52,23 @@ Type: String
 Parameter Sets: (All)
 Aliases:
 
-Required: True
+Required: False
 Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AmazonResourceName
+Specifies amazon resource name.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 2
 Default value: None
 Accept pipeline input: False
 Accept wildcard characters: False
@@ -45,8 +82,8 @@ Type: String
 Parameter Sets: (All)
 Aliases:
 
-Required: True
-Position: 2
+Required: False
+Position: 3
 Default value: None
 Accept pipeline input: False
 Accept wildcard characters: False
@@ -60,8 +97,113 @@ Type: String
 Parameter Sets: (All)
 Aliases:
 
-Required: True
-Position: 3
+Required: False
+Position: 4
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AuthMethod
+Specifies the authentication method.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 5
+Default value: KUseIAMUser
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AWSType
+Specifies the AWS type.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 6
+Default value: KIAMUser
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -SubscriptionType
+Specifies the subscription type.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 7
+Default value: KAWSCommercial
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FleetSubnetType
+Fleet settings, fleet subnet type.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 8
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FleetTags
+Fleet settings, fleet tags.
+
+```yaml
+Type: Object[]
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 9
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AccountId
+AWS account id.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 10
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -IamRoleName
+IamRole registration name.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 11
 Default value: None
 Accept pipeline input: False
 Accept wildcard characters: False
--- src/Cohesity.Powershell/Scripts/ProtectionSource/Register-CohesityProtectionSourceAWS.ps1
+++ src/Cohesity.Powershell/Scripts/ProtectionSource/Register-CohesityProtectionSourceAWS.ps1
@@ -10,21 +10,64 @@ function Register-CohesityProtectionSourceAWS {
         https://cohesity.github.io/cohesity-powershell-module/#/README
         .EXAMPLE
         Register-CohesityProtectionSourceAWS -AccessKey "access-key" -SecretAccessKey "secret-key" -ARN "aws-arn"
+        .EXAMPLE
+        Register-CohesityProtectionSourceAWS -AccessKey access-key -SecretAccessKey "secret-key" -ARN "aws-arn" -FleetSubnetType kSourceVM -FleetTags $tags
+        Construct the tags as follows,
+        $tags = @()
+        $tags += @{key="bjt1";value="bjv1"}
+        $tags += @{key="bjt2";value="bjv2"}
+        .EXAMPLE
+        Register-CohesityProtectionSourceAWS -AuthMethod kUseIAMRole -IamRoleName "role-name" -AccountId "12321432" -FleetSubnetType kSourceVM -FleetTags $tags
+        Register AWS protection source with IAmRole authentication method, construct the tags as follows,
+        $tags = @()
+        $tags += @{key="bjt1";value="bjv1"}
+        $tags += @{key="bjt2";value="bjv2"}
     #>
   [CmdletBinding()]
   Param(
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     # Specifies the access key.
     [String]$AccessKey,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
+    # Specifies amazon resource name.
+    [String]$AmazonResourceName,
+    [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     # Specifies the secret access key.
     [String]$SecretAccessKey,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     # Specifies the ARN.
-    [String]$ARN
+    [String]$ARN,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kUseIAMUser", "kUseIAMRole")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the authentication method.
+    [String]$AuthMethod = "kUseIAMUser",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kIAMUser", "kRegion", "kAvailabilityZone", "kEC2Instance", "kVPC", "kSubnet", "kNetworkSecurityGroup", "kInstanceType", "kKeyPair", "kTag", "kRDSOptionGroup", "kRDSParameterGroup", "kRDSInstance", "kRDSSubnet", "kRDSTag", "kAuroraCluster")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the AWS type.
+    [String]$AWSType = "kIAMUser",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kAWSCommercial", "kAWSGovCloud")]
+    [ValidateNotNullOrEmpty()]
+    # Specifies the subscription type.
+    [String]$SubscriptionType = "kAWSCommercial",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("kCluster", "kSourceVM", "kCustom")]
+    # Fleet settings, fleet subnet type.
+    [String]$FleetSubnetType,
+    [Parameter(Mandatory = $false)]
+    # Fleet settings, fleet tags.
+    [object[]]$FleetTags,
+    [Parameter(Mandatory = $false)]
+    # AWS account id.
+    [string]$AccountId,
+    [Parameter(Mandatory = $false)]
+    # IamRole registration name.
+    [string]$IamRoleName
   )
 
   Begin {
@@ -32,20 +75,76 @@ function Register-CohesityProtectionSourceAWS {
 
   Process {
 
-    $uri =  '/irisservices/api/v1/public/protectionSources/register'
+    if ($AuthMethod -eq "kUseIAMUser") {
+        $uri = '/irisservices/api/v1/public/protectionSources/register'
 
-    $awsRegistrationParameters = @{
-      awsCredentials = @{
-        accessKey       = $AccessKey
-        authMethod      = "kUseIAMUser"
-        awsType         = "kIAMUser"
-        iamRoleArn      = $ARN
-        secretAccessKey = $SecretAccessKey
-      }
-      environment    = "kAWS"
+        $awsRegistrationParameters = @{
+          awsCredentials = @{
+            accessKey          = $AccessKey
+            amazonResourceName = $AmazonResourceName
+            authMethod         = $AuthMethod
+            awsType            = $AWSType
+            iamRoleArn         = $ARN
+            secretAccessKey    = $SecretAccessKey
+            subscriptionType   = $SubscriptionType
+          }
+          environment    = "kAWS"
+        }
+        if ($FleetSubnetType) {
+            $awsFleetParams = @{
+            fleetSubnetType = $FleetSubnetType
+            fleetTags       = $FleetTags
+            }
+            $awsRegistrationParameters | Add-Member -NotePropertyName awsFleetParams -NotePropertyValue $awsFleetParams
+        }
+
+    } else {
+        # the auth method would be kUseIAMRole
+        $uri = '/irisservices/api/v1/backupsources'
+
+        $ENTITY_TYPE = 16
+        $AWS_ENTITY_TYPE = 0
+        $ENTITY_INFO_TYPE = 16
+        $AWS_AUTH_METHOD = 1
+        $SUBSCRIPTION_TYPE = 3
+        $iamRoleARN = [string]::Format("arn:aws:iam::{0}:role/{1}",$AccountId,$IamRoleName)
+        $awsFleetParams = $null
+        if ($FleetSubnetType) {
+            $awsFleetParams = @{
+                fleetSubnetType = $FleetSubnetType
+                fleetTagVec       = $FleetTags
+            }
+        }
+        $awsEntity = @{
+                    type =  $AWS_ENTITY_TYPE
+                    ownerId = $AccountId
+                    commonInfo = @{
+                        id = $AccountId
+                    }
+                    awsFleetParams = $awsFleetParams
+                }
+
+        $awsRegistrationParameters = @{
+            entity = @{
+                type = $ENTITY_TYPE
+                awsEntity = $awsEntity
+            }
+            entityInfo = @{
+                type = $ENTITY_INFO_TYPE
+                credentials = @{
+                    cloudCredentials = @{
+                        awsCredentials = @{
+                            authMethod = $AWS_AUTH_METHOD
+                            subscriptionType = $SUBSCRIPTION_TYPE
+                            iamRoleArn = $iamRoleARN
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    $request = $awsRegistrationParameters | ConvertTo-Json
+    $request = $awsRegistrationParameters | ConvertTo-Json -Depth 100
     $result = Invoke-RestApi -Method Post -Uri $uri -Body $request
     $result
   }
