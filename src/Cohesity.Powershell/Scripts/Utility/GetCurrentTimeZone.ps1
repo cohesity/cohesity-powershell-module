b788dfd6fceefe3133f95f8a997d71b2caa86c89
Awneesh Singh <naveena.maplelabs@corp.cohesity.com>
2023-05-16 18:59:50 +0500

    Removed comments

--- src/Cohesity.Powershell/Scripts/Utility/GetCurrentTimeZone.ps1
+++ src/Cohesity.Powershell/Scripts/Utility/GetCurrentTimeZone.ps1
@@ -6,14 +6,19 @@ function GetCurrentTimeZone {
     [string]$timeZone = "America/Los_Angeles"
     try {
         $standardTZ = (Get-TimeZone).Id
+        $current_dir = pwd
+        $tz_file = [IO.Path]::Combine($current_dir.Path, "timezone_mapping.json")
         # find out if its a Windows OS
         [bool]$itsWindows = [System.Environment]::OSVersion.VersionString -like "*Windows*"
         if ($itsWindows) {
             # need to convert the time zone for windows (for consistency)
             # Ref : https://devblogs.microsoft.com/dotnet/cross-platform-time-zones-with-net-core/
             # $standardTZ = [TimeZoneConverter.TZConvert]::WindowsToIana($standardTZ)
-            $tz = get-content timezone_mapping.json | ConvertFrom-Json
+            $tz = get-content $tz_file | ConvertFrom-Json
+            $time_zone = $tz.$standardTZ
+            if ($time_zone -ne $null){
             $standardTZ = $tz.$standardTZ
+        }
         }
         $timeZone = $standardTZ
     }
